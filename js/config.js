00f52acd3ed1544e7724e0957c5f25b2fdf1ae2f
insceptions <41789133+insceptions@users.noreply.github.com>
2018-07-29 02:45:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ var l_displayMapGamemode = true;
 
 // Custom server name configuration
 // Leave it blank for automatic server name
-var l_serverName = "Insceptions Imperial Roleplay";
+var l_serverName = "Insception's;
 
 // Use a video for the background?
 // Otherwise it will use images
